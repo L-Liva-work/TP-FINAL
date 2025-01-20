287351ccb1355ae688a6a5139ec14a9d59bd98f8
JaelMC <jmamanic@fi.uba.ar>
2025-01-19 21:33:37 -0300

    Add route for managing persons

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express')
+const persons = require('./routes/persons')
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Organizador de tareas')
 })
+
+app.use('/api/v1/persons', persons)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
