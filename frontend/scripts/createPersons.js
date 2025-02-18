d741b375364e51e350ba15b1ddbea2eca7ca4c03
JaelMC <jmamanic@fi.uba.ar>
2025-02-18 00:12:29 -0300

    Add validations for password

--- frontend/scripts/createPersons.js
+++ frontend/scripts/createPersons.js
@@ -1,17 +1,20 @@
 const regex_nombre = /^[a-zA-Z\s]{1,30}$/ 
 const regex_email =  /^[a-zA-z0-9_.]+@[a-zA-Z0-9]+\.[a-zA-Z0-9_.+-]+$/
+const regex_password = /^.{4,12}$/
 const regex_doc = /^[0-9]{1,8}$/
 const regex_telefono = /^[0-9\s]{1,12}$/
 const regex_puesto = /^[a-zA-z0-9\s]+$/      
       
 const nombre = document.getElementById('nombre')
 const email = document.getElementById('email')
+const password = document.getElementById('password')
 const doc = document.getElementById('documento')
 const telefono = document.getElementById('telefono')
 const puesto = document.getElementById('puesto')
 
 nombre.addEventListener('keyup', () => validar_campo(nombre, regex_nombre, 'error-nombre'))
 email.addEventListener('keyup', () => validar_campo(email, regex_email, 'error-email'))
+password.addEventListener('keyup' , () =>validar_campo(password,regex_password, 'error-password') )
 doc.addEventListener('keyup', () => validar_campo(doc, regex_doc, 'error-documento'))
 telefono.addEventListener('keyup', () => validar_campo(telefono, regex_telefono, 'error-telefono'))
 puesto.addEventListener('keyup', () => validar_campo(puesto, regex_puesto, 'error-puesto'))
@@ -39,6 +42,9 @@ function validar_formulario() {
     if (!regex_email.test(email.value)){
         es_valido = false
     }
+    if (!regex_password.test(password.value)){
+      es_valido = false
+    }
     if (!regex_doc.test(doc.value)){
         es_valido = false
     }
@@ -55,6 +61,7 @@ function create_person() {
     event.preventDefault()
     const nombre = document.getElementById('nombre').value 
     const email = document.getElementById('email').value 
+    const password = document.getElementById('password').value
     const doc = document.getElementById('documento').value 
     const puesto = document.getElementById('puesto').value
     const telefono = document.getElementById('telefono').value
@@ -62,6 +69,7 @@ function create_person() {
       let body =  {
           email: email,
           nombre: nombre,
+          password: password,
           doc: parseInt(doc),
           puesto: puesto,
           telefono: parseInt(telefono)
@@ -78,7 +86,8 @@ function create_person() {
           console.log(response.status)
           if (response.status === 201) {
               alert('Persona creada con exito') 
-              window.location.href = "persons.html";
+              //window.location.href = "persons.html"
+              window.location.href = "login.html"
 
               limpiar_formulario()
                 
