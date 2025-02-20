fc3e399a35dadf23455708a6d7b3fa3b942c2c9e
JaelMC <jmamanic@fi.uba.ar>
2025-02-19 22:01:41 -0300

    Fix for extracting token

--- backend/src/middlewares/verifyToken.js
+++ backend/src/middlewares/verifyToken.js
@@ -2,13 +2,19 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = async (req , res, next ) =>{
-    const token = req.headers['token']
-    console.log(token)
+    const authHeader = req.headers['authorization']
+    if (!authHeader) {
+        return res.status(400).json({ error: 'No se envió el token' })
+    }
+    console.log('Authorization header recibido:', authHeader)
+    const token = authHeader.split(' ')[1]
+    console.log('Token extraido: '+token)
     if (token) {
         jwt.verify(token, "extra-secreto", (error, data)=>{
             if(error){
                 return res.status(400).json('Token invalido')
             } else {
+                //console.log('Token verificado', data)
                 req.person = data
                 next()
             }
